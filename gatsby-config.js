aa8149be2c3643c78d9bbbc8a4fb574787759452
Blake Hagen <blakehagen@gmail.com>
2019-02-09 14:55:23 -0700

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    // {
+    //   resolve: `gatsby-plugin-google-fonts`,
+    //   options: {
+    //     fonts: [`roboto:100,300,400,700`],
+    //   },
+    // },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+const App = () => (
+  <div className={styles.appWrapper}>
+    {/* Header here... */}
+    <div>
+     cool app bro!
+    </div>
+    {/* Footer here... */}
+  </div>
+);
+
+export default App;
